f752a42020166d507d2c347cc61538621375daaf
spencer-rafada <spencer.byui@gmail.com>
2022-11-28 16:42:46 -0700

    added move collections

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Admin, Dashboard } from "./views/index.js";
+import { Admin, Dashboard, Move } from "./views/index.js";
 import NavBar from "./components/layout/NavBar.js";
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Dashboard />}></Route>
           <Route exact path="/admin" element={<Admin />}></Route>
+          <Route exact path="/move" element={<Move />}></Route>
         </Routes>
       </Router>
     </div>
