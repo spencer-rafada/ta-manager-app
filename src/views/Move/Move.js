b43619c78bd7fab06c8c3861a5e1a75df534a10f
spencer-rafada <spencer.byui@gmail.com>
2022-11-28 16:42:58 -0700

    move collection

---
+++ src/views/Move/Move.js
@@ -0,0 +1,10 @@
+import React from "react";
+import MoveCollection from "../../components/MoveCollection/MoveCollection.js";
+
+export default function Move() {
+  return (
+    <div>
+      <MoveCollection />
+    </div>
+  );
+}
